e1bc3b0a4301bc92e1babcfbabe55f1ce2fa73ad
LogicalAnt <shakil147258@gmail.com>
2023-06-13 03:56:10 +0600

    feat: :sparkles: add color mapper constants

---
+++ src/theme/mapper/index.ts
@@ -0,0 +1,5 @@
+export { default as bg } from "./bg"
+export { default as border } from "./border"
+export { default as hover } from "./hover"
+export { default as outline } from "./outline"
+export { default as text } from "./text"
