11619a90589aae2dc7171429b310ae27e6faac9d
sakilk130 <sakilk130@gmail.com>
2023-06-15 23:52:41 +0600

    test: :test_tube: add test config & test Tag component

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    ".(css|less|scss)$": "identity-obj-proxy",
+    '.(css|less|scss)$': 'identity-obj-proxy',
   },
 };
