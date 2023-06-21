33168d54432bc72487fd420851c7bf9a052a2641
LogicalAnt <shakil147258@gmail.com>
2023-06-21 13:26:11 +0600

    feat: :sparkles: add rating component

---
+++ src/components/atoms/Rating/Rating.tsx
@@ -0,0 +1,46 @@
+import React, { forwardRef, useState } from "react"
+import { Shapes, maxStar, minStar } from "./utils/constants"
+import { container, sizeMapper, svgItem } from "./utils/styles"
+import { RatingProps } from "./utils/types"
+
+const Rating: React.FC<RatingProps> = forwardRef<HTMLDivElement, RatingProps>(
+  function Rating(
+    { color = "yellow", size = "sm", variant = "heart", count = 5 },
+    ref
+  ) {
+    const [rating, setRating] = useState(0)
+
+    count = Math.min(maxStar, Math.max(minStar, count))
+
+    return (
+      <div className={container} ref={ref}>
+        {[...Array(count)].map((_, index) => {
+          const starValue = index + 1
+
+          return (
+            <svg
+              viewBox="0 0 16 14"
+              key={index}
+              width={sizeMapper[size]}
+              height={sizeMapper[size]}
+              className={svgItem({ color })}
+              onClick={() => {
+                setRating(starValue == rating ? index : starValue)
+              }}
+            >
+              <path
+                d={
+                  starValue <= rating
+                    ? Shapes[variant].filled
+                    : Shapes[variant].outlined
+                }
+              />
+            </svg>
+          )
+        })}
+      </div>
+    )
+  }
+)
+
+export default Rating
---
+++ src/components/atoms/Rating/utils/styles.ts
@@ -0,0 +1,16 @@
+import classNames from "classnames"
+import { Colors, Size } from "../../../../theme/constants"
+import { fill, hoverFill } from "../../../../theme/mapper"
+
+const sizeMapper: Record<Size, number> = {
+  lg: 32,
+  md: 24,
+  sm: 16,
+}
+
+const container = classNames(["flex", "items-center", "gap-2"])
+const svgItem = ({ color }: { color: Colors }) => {
+  return classNames([fill[color][500], hoverFill[color][400]])
+}
+
+export { container, sizeMapper, svgItem }
