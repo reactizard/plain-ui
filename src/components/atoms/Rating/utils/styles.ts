a9d0c2085d03c626367bfa5c6a6ac90b8a666b1f
LogicalAnt <shakil147258@gmail.com>
2023-06-28 20:39:18 +0600

    feat: :sparkles: add a simple animation effect on star component

--- src/components/atoms/Rating/utils/styles.ts
+++ src/components/atoms/Rating/utils/styles.ts
@@ -10,7 +10,10 @@ const sizeMapper: Record<Size, number> = {
 
 const container = classNames(["flex", "items-center", "gap-2"])
 const svgItem = ({ color }: { color: Colors }) => {
-  return classNames([fill[color][500], hoverFill[color][400]])
+  return classNames(
+    [fill[color][500], hoverFill[color][400]],
+    "hover:-translate-y-[0.08rem]"
+  )
 }
 
 export { container, sizeMapper, svgItem }
