3ab77f930d286944f9f6948965d83c30a8cda7e9
LogicalAnt <shakil147258@gmail.com>
2023-06-28 15:01:19 +0600

    refactor: :recycle: remove icons from label storybook

--- src/components/atoms/Label/Label.stories.tsx
+++ src/components/atoms/Label/Label.stories.tsx
@@ -1,34 +1,10 @@
 import { Meta, StoryObj } from "@storybook/react"
-import {
-  IconChevronUp,
-  IconMessageCircle2,
-  IconMoodBoy,
-  IconPlus,
-} from "@tabler/icons-react"
-import React from "react"
 import Label from "./Label"
-
-const icons = {
-  Message: <IconMessageCircle2 />,
-  Plus: <IconPlus />,
-  MoodBoy: <IconMoodBoy />,
-  ChevronUp: <IconChevronUp />,
-  Hello: "Hello",
-}
 
 const meta: Meta<typeof Label> = {
   title: "Atoms/Label",
   component: Label,
   tags: ["autodocs"],
-  argTypes: {
-    text: {
-      options: Object.keys(icons),
-      mapping: icons,
-      control: {
-        type: "select",
-      },
-    },
-  },
 }
 
 export default meta
