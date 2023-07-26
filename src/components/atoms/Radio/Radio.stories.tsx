37ac7364bd3a7addca911e33167b92f4d8b6fe27
LogicalAnt <shakil147258@gmail.com>
2023-07-26 19:26:14 +0600

    feat: :sparkles: add radio stories

---
+++ src/components/atoms/Radio/Radio.stories.tsx
@@ -0,0 +1,71 @@
+import { Meta, StoryObj } from "@storybook/react"
+import React from "react"
+import Radio from "./Radio"
+import { RadioGroupProps } from "./utils/types"
+
+type Story = StoryObj<RadioGroupProps>
+
+const meta: Meta<RadioGroupProps> = {
+  title: "Atoms/Radio",
+  component: Radio.Group,
+  tags: ["autodocs"],
+}
+
+const GroupTemplate: Story = {
+  argTypes: {
+    size: { defaultValue: { summary: "md" } },
+    direction: { defaultValue: { summary: "column" } },
+  },
+  render: ({ items, ...args }: any) => {
+    return (
+      <Radio.Group
+        name={args.name}
+        size={args.size}
+        direction={args.direction}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          console.log("radio:", e.target.value)
+        }}
+      >
+        {items.map((item: object, index: number) => (
+          <Radio {...item} key={index} />
+        ))}
+      </Radio.Group>
+    )
+  },
+}
+
+export const GroupRadio = {
+  ...GroupTemplate,
+  args: {
+    items: [
+      {
+        label: "Radio one",
+        value: 1,
+        color: "lime",
+      },
+      { label: "Radio two", value: 2 },
+      { label: "Radio three", value: 3 },
+    ],
+    direction: "column",
+    name: "radio",
+    size: "md",
+  },
+}
+
+export const RadioSingle: Story = {
+  parameters: {},
+  name: "Single Radio",
+  render: () => (
+    <>
+      <Radio label="Radio Single" color="primary" labelPosition="left" />
+      <Radio
+        label="Radio Single"
+        color="primary"
+        labelPosition="left"
+        disabled
+      />
+    </>
+  ),
+}
+
+export default meta
