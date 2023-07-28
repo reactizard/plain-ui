8ca848799ba30ab2b70b65c562ed21f273f028af
LogicalAnt <shakil147258@gmail.com>
2023-07-28 22:41:11 +0600

    feat: :sparkles: add initial Link component

---
+++ src/components/atoms/Link/Link.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { Link } from "./Link"
+
+const meta: Meta<typeof Link> = {
+  title: "Atoms/Link",
+  component: Link,
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof Link>
+
+export const Primary: Story = {
+  args: {
+    href: "https://www.google.com",
+    text: "Google",
+    color: "danger",
+    underline: true,
+  },
+}
