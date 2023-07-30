f467c4c6369f3e0654bef26f92f9a25e93cbb13a
LogicalAnt <shakil147258@gmail.com>
2023-07-30 13:12:39 +0600

    feat: :sparkles: update Link props and styles

--- src/components/atoms/Link/Link.tsx
+++ src/components/atoms/Link/Link.tsx
@@ -1,19 +1,19 @@
-import { forwardRef } from "react"
+import { FC, forwardRef } from "react"
 
 import React from "react"
 import { getStyles } from "./utils/styles"
 import { LinkProps } from "./utils/types"
 
-export const Link = forwardRef<HTMLAnchorElement, LinkProps>(function Link(
-  { href, text, color, underline },
-  ref
-) {
-  const { linkCls } = getStyles({ color, underline })
-  return (
-    <a href={href} ref={ref} className={linkCls}>
-      {text}
-    </a>
-  )
-})
+export const Link: FC<LinkProps> = forwardRef<HTMLAnchorElement, LinkProps>(
+  function Link(props, ref) {
+    const { href, text, color, size, underline, ...rest } = props
+    const { linkCls } = getStyles({ color, size, underline })
+    return (
+      <a href={href} ref={ref} className={linkCls} {...rest}>
+        {text}
+      </a>
+    )
+  }
+)
 
 export default Link
--- src/components/atoms/Link/utils/styles.ts
+++ src/components/atoms/Link/utils/styles.ts
@@ -1,11 +1,23 @@
 import classNames from "classnames"
+import { Size } from "../../../../theme/constants"
 import { text } from "../../../../theme/mapper"
 import { StyleProps } from "./types"
 
-export const getStyles = ({ color, underline }: StyleProps) => {
+export const getStyles = ({
+  color,
+  size = "md",
+  underline = true,
+}: StyleProps) => {
+  const sizeMapper: Record<Size, string> = {
+    sm: "text-smRegular",
+    md: "text-mdRegular",
+    lg: "text-lgRegular",
+  }
+
   const linkCls = classNames([
-    text[color][500],
+    color ? text[color][500] : "",
     underline ? "underline" : "no-underline",
+    sizeMapper[size],
   ])
 
   return { linkCls }
