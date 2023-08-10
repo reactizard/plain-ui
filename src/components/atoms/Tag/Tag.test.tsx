2c6450e121b822dd081f249bc0addf59c6beea13
LogicalAnt <shakil147258@gmail.com>
2023-08-10 10:09:51 +0600

    style: :rotating_light: fix linter warning

--- src/components/atoms/Tag/Tag.test.tsx
+++ src/components/atoms/Tag/Tag.test.tsx
@@ -1,31 +1,31 @@
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen } from '@testing-library/react';
-import React from 'react';
+import "@testing-library/jest-dom/extend-expect"
+import { render, screen } from "@testing-library/react"
+import React from "react"
 
-import Tag, { TagProps } from './Tag';
+import Tag, { TagProps } from "./Tag"
 
-describe('Tag component', () => {
+describe("Tag component", () => {
   const testProps: TagProps = {
-    text: 'Test Tag',
-    variant: 'filled',
-    color: 'primary',
-  };
+    text: "Test Tag",
+    variant: "filled",
+    color: "primary",
+  }
 
-  test('renders tag text correctly', () => {
-    render(<Tag {...testProps} />);
-    const tagElement = screen.getByText(testProps.text);
-    expect(tagElement).toBeInTheDocument();
-  });
+  test("renders tag text correctly", () => {
+    render(<Tag {...testProps} />)
+    const tagElement = screen.getByText(testProps.text)
+    expect(tagElement).toBeInTheDocument()
+  })
 
-  test('applies the correct variant class', () => {
-    render(<Tag {...testProps} />);
-    const tagElement = screen.getByText(testProps.text);
-    expect(tagElement).toHaveClass('bg-primary-500', 'text-white');
-  });
+  test("applies the correct variant class", () => {
+    render(<Tag {...testProps} />)
+    const tagElement = screen.getByText(testProps.text)
+    expect(tagElement).toHaveClass("bg-primary-500", "text-white")
+  })
 
-  test('applies the correct color class', () => {
-    render(<Tag {...testProps} />);
-    const tagElement = screen.getByText(testProps.text);
-    expect(tagElement).toHaveClass('bg-primary-500');
-  });
-});
+  test("applies the correct color class", () => {
+    render(<Tag {...testProps} />)
+    const tagElement = screen.getByText(testProps.text)
+    expect(tagElement).toHaveClass("bg-primary-500")
+  })
+})
