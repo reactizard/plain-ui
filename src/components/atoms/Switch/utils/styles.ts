495fb0c7a94ae288b131c8c9053fc29d4f886d4f
LogicalAnt <shakil147258@gmail.com>
2023-08-12 03:28:28 +0600

    feat: :lipstick: update style according to the UI

--- src/components/atoms/Switch/utils/styles.ts
+++ src/components/atoms/Switch/utils/styles.ts
@@ -1,25 +1,32 @@
 import classNames from "classnames"
+import { bg, border, hover } from "../../../../theme/mapper"
 import { StyleProps } from "./types"
 
 export const getStyles = ({ color, disable, labelPosition }: StyleProps) => {
-  const inputCls = classNames(["h-0", "w-0", "appearance-none"])
+  const inputCls = classNames(["pUISwitch", "h-0", "w-0", "appearance-none"])
   const labelCls = classNames([
     "cursor-pointer",
     "-indent-[9999px]",
-    "w-[50px]",
-    "h-[25px]",
+    "w-[40px]",
+    "h-[24px]",
     "block",
     "rounded-[20px]",
-    "bg-gray-500",
+    bg[color][300],
+    hover[color][400],
     "relative",
     "after:content-['']",
     "after:absolute",
-    "after:bg-red-500",
-    "after:w-[18px]",
-    "after:h-[18px]",
-    "after:top-[3px]",
-    "after:left-[3px]",
+    "after:bg-gray-100",
+    "after:w-[20px]",
+    "after:h-[20px]",
+    "after:top-[1px]",
+    "after:left-[1px]",
     "after:rounded-[10px]",
+    "checked:bg-blue-500",
+    "border-[1px]",
+    border[color][200],
+    "after:transition-all",
+    "after:duration-300",
   ])
   return { inputCls, labelCls }
 }
