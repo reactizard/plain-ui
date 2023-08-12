6d8709fa0ebdc8971fe88519b6a2e076792447a8
LogicalAnt <shakil147258@gmail.com>
2023-08-13 02:33:29 +0600

    feat: :sparkles: add size, text, sub text on Switch component

--- src/components/atoms/Switch/utils/styles.ts
+++ src/components/atoms/Switch/utils/styles.ts
@@ -1,32 +1,57 @@
 import classNames from "classnames"
+import { Size } from "../../../../theme/constants"
 import { bg, border, hover } from "../../../../theme/mapper"
 import { StyleProps } from "./types"
 
-export const getStyles = ({ color, disable, labelPosition }: StyleProps) => {
+export const getStyles = ({
+  color,
+  size,
+  checked,
+  labelPosition,
+}: StyleProps) => {
+  const sizeMapper: Record<Size, string> = {
+    lg: "w-[60px] h-[34px] after:w-[30px] after:h-[30px]",
+    md: "w-[40px] h-[24px] after:w-[20px] after:h-[20px]",
+    sm: "w-[30px] h-[14px] after:w-[10px] after:h-[10px]",
+  }
+
+  const containerCls = classNames([
+    "max-w-fit",
+    "items-center",
+    "flex",
+    labelPosition == "left" ? "flex-row-reverse" : "flex-row",
+  ])
   const inputCls = classNames(["pUISwitch", "h-0", "w-0", "appearance-none"])
   const labelCls = classNames([
     "cursor-pointer",
     "-indent-[9999px]",
-    "w-[40px]",
-    "h-[24px]",
+    size ? sizeMapper[size] : "",
     "block",
-    "rounded-[20px]",
-    bg[color][300],
-    hover[color][400],
+    "rounded-full",
+    color && checked ? bg[color][300] : bg["gray"][300],
+    color && checked ? hover[color][400] : bg["gray"][400],
     "relative",
     "after:content-['']",
     "after:absolute",
     "after:bg-gray-100",
-    "after:w-[20px]",
-    "after:h-[20px]",
     "after:top-[1px]",
     "after:left-[1px]",
-    "after:rounded-[10px]",
-    "checked:bg-blue-500",
+    "after:rounded-full",
     "border-[1px]",
-    border[color][200],
+    color && checked ? border[color][200] : border["gray"][200],
     "after:transition-all",
     "after:duration-300",
   ])
-  return { inputCls, labelCls }
+  const textContainerCls = classNames(["mx-2", "flex", "flex-col", ""])
+  const textCls = classNames(["text-mdSemiBold"])
+  const subTextCls = classNames(["text-smRegular"])
+
+  return {
+    containerCls,
+    inputCls,
+    labelCls,
+    textContainerCls,
+    textCls,
+    subTextCls,
+  }
 }
