1cab7a9e372c225a5cdb096a8a6742282f70737a
LogicalAnt <shakil147258@gmail.com>
2023-08-18 03:10:45 +0600

    refactor: :recycle: divide exports by comment

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+// Atoms export
 export { default as Button } from "./atoms/Button"
 export { default as Checkbox } from "./atoms/Checkbox"
 export { default as Chip } from "./atoms/Chip"
@@ -10,3 +11,6 @@ export { default as Radio } from "./atoms/Radio"
 export { default as Switch } from "./atoms/Switch"
 export { default as Tag } from "./atoms/Tag"
 export { default as Tooltip } from "./atoms/Tooltip"
+
+// Molecules export
+export { default as Alert } from "./molecules/Alert"
