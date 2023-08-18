c043470778dd29275190304c2039f4f3fabc5832
LogicalAnt <shakil147258@gmail.com>
2023-08-19 03:38:46 +0600

    style: :lipstick: add initial UI style for Alert component

--- src/components/molecules/Alert/utils/styles.ts
+++ src/components/molecules/Alert/utils/styles.ts
@@ -1,15 +1,34 @@
 import classNames from "classnames"
+import { Colors } from "../../../../theme/constants"
+import { bg, text } from "../../../../theme/mapper"
 import { StyleProps } from "./types"
 
 export const getStyles = ({ color }: StyleProps) => {
+  const borderColorMapper: Record<Colors, string> = {
+    aloha: "border-l-aloha-500",
+    azure: "border-l-azure-500",
+    danger: "border-l-danger-500",
+    gray: "border-l-gray-500",
+    lime: "border-l-lime-500",
+    melon: "border-l-melon-500",
+    orange: "border-l-orange-500",
+    pink: "border-l-pink-500",
+    primary: "border-l-primary-500",
+    purple: "border-l-purple-500",
+    success: "border-l-success-500",
+    warm: "border-l-warm-500",
+    warning: "border-l-warning-500",
+    yellow: "border-l-yellow-500",
+  }
   const container = classNames([
     "container",
     "h-[154px]",
-    "w-[500px]",
+    "w-[300px]",
+    "sm:w-[500px]",
     "rounded-[5px]",
     "border-l-[4px]",
-    "border-l-indigo-800",
-    "bg-blue-300",
+    borderColorMapper[color],
+    bg[color][100],
   ])
   const body = classNames([
     "body",
@@ -17,32 +36,58 @@ export const getStyles = ({ color }: StyleProps) => {
     "flex-col",
     "items-start",
     "gap-[8px]",
-    "px-[5px]",
-    "py-[8px]",
+    "p-[16px]",
   ])
-  const rlDivider = classNames(["rl-divider", "flex", "flex-row"])
-  const leftPart = classNames(["left", "px-[5px]"])
+  const rlDivider = classNames([
+    "rl-divider",
+    "flex",
+    "flex-col",
+    "sm:flex-row",
+  ])
+  const leftPart = classNames(["left", "mr-[5px]"])
   const rightPart = classNames(["right", "flex", "flex-col", "gap-[5px]"])
-  const header = classNames(["header", "flex", "w-full", "justify-between"])
-  const title = classNames([])
+  const headerContainer = classNames([
+    "headerContainer",
+    "flex",
+    "w-full",
+    "justify-between",
+  ])
+  const title = classNames(["max-w-[350px]", "truncate"])
   const closer = classNames([
     "closer",
-    "rounded-full",
-    "font-bold",
-    "text-white",
+    "flex",
+    "w-[24px]",
+    "h-[24px]",
+    "p-[6px]",
+    "justify-center",
+    "items-center",
+    "text-smRegular",
+    text["gray"][600],
+    "-translate-y-[22px]",
+    "md:-translate-y-[0px]",
   ])
-  const content = classNames(["content", "text-mdRegular"])
-  const footer = classNames([])
+  const description = classNames([
+    "max-h-[50px]",
+    "sm:max-h-[70px]",
+    "min-w-[240px]",
+    "sm:min-w-[440px]",
+    "max-w-[280px]",
+    "sm:max-w-[440px]",
+    "description",
+    "text-mdRegular",
+    "text-ellipsis overflow-auto",
+  ])
+  const footer = title
   return {
     container,
     body,
     rlDivider,
     leftPart,
     rightPart,
-    header,
+    headerContainer,
     title,
     closer,
-    content,
+    description,
     footer,
   }
 }
