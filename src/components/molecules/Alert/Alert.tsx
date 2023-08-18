cdcd2f5270f3446add47e071e9d1ebba091958f1
LogicalAnt <shakil147258@gmail.com>
2023-08-19 05:49:53 +0600

    feat: :sparkles: apply framer motion on alert component

--- src/components/molecules/Alert/Alert.tsx
+++ src/components/molecules/Alert/Alert.tsx
@@ -1,5 +1,7 @@
 import { IconX } from "@tabler/icons-react"
+import { AnimatePresence, motion } from "framer-motion"
 import React, { FC, forwardRef } from "react"
+import useDisclosure from "../../../hooks/useDisclosure"
 import { getStyles } from "./utils/styles"
 import { AlertProps } from "./utils/types"
 
@@ -15,25 +17,36 @@ export const Alert: FC<AlertProps> = forwardRef<HTMLDivElement, AlertProps>(
     },
     ref
   ) {
+    const [opened, { close }] = useDisclosure(true)
     const styles = getStyles({ color })
     return (
-      <div className={styles.container} ref={ref}>
-        <div className={styles.body}>
-          <div className={styles.rlDivider}>
-            <div className={styles.leftPart}>{sideIcon}</div>
-            <div className={styles.rightPart}>
-              <div className={styles.headerContainer}>
-                <div className={styles.title}>{title}</div>
-                <div className={styles.closer}>
-                  {closable ? <IconX /> : null}
+      <AnimatePresence>
+        {opened ? (
+          <motion.div
+            className={styles.container}
+            ref={ref}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, transition: { duration: 0.5 } }}
+          >
+            <div className={styles.body}>
+              <div className={styles.rlDivider}>
+                <div className={styles.leftPart}>{sideIcon}</div>
+                <div className={styles.rightPart}>
+                  <div className={styles.headerContainer}>
+                    <div className={styles.title}>{title}</div>
+                    <div className={styles.closer} onClick={close}>
+                      {closable ? <IconX /> : null}
+                    </div>
+                  </div>
+                  <p className={styles.description}>{description}</p>
+                  <div className={styles.footer}>{footer}</div>
                 </div>
               </div>
-              <p className={styles.description}>{description}</p>
-              <div className={styles.footer}>{footer}</div>
             </div>
-          </div>
-        </div>
-      </div>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
     )
   }
 )
