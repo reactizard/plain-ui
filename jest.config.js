64f81b8a92b1cfa9e270af97fac7d1f91fcb4345
LogicalAnt <shakil147258@gmail.com>
2023-08-27 18:22:05 +0600

    refactor: :recycle: update jest config to collect coverage from the right files & directory

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
-    '.(css|less|scss)$': 'identity-obj-proxy',
+    ".(css|less|scss)$": "identity-obj-proxy",
   },
-};
+  "collectCoverageFrom": [
+    "src/**/*.{ts,tsx}",
+    "!<rootDir>/src/**/*/*.stories.{ts,tsx}",
+    "!<rootDir>/src/**/*/{styles,types,index}.{ts,tsx}"
+  ]
+}
