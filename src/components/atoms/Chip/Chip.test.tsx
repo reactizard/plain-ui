c1f9bf223cdf2bab97391fe33624290f973bc06d
Shakil Ahmed <shakil147258@gmail.com>
2023-08-27 18:24:40 +0600

    feat: :sparkles: add Breadcrumb component
    * feat: :sparkles: add initial Breadcrumb component
    
    * fix: :bug: add some major fixes on Breadcrumb component
    
    * test: :test_tube: add Breadcrumb component test, fix linting on Chip test file
    
    * test: :test_tube: add test for Link component
    
    * refactor: :recycle: update jest config to collect coverage from the right files & directory
    
    * refactor: :recycle: workflow will use the jest config
    
    instead the inline command

--- src/components/atoms/Chip/Chip.test.tsx
+++ src/components/atoms/Chip/Chip.test.tsx
@@ -1,39 +1,39 @@
-import '@testing-library/jest-dom/extend-expect';
-import React from 'react';
-import { render } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect"
+import { render } from "@testing-library/react"
+import React from "react"
 
-import Chip from './Chip';
+import Chip from "./Chip"
 
-describe('Chip component', () => {
-  test('renders properly', () => {
-    const { getByText } = render(<Chip text="Test Chip" />);
-    const chipText = getByText('Test Chip');
-    expect(chipText).toBeInTheDocument();
-  });
+describe("Chip component", () => {
+  test("renders properly", () => {
+    const { getByText } = render(<Chip text="Test Chip" />)
+    const chipText = getByText("Test Chip")
+    expect(chipText).toBeInTheDocument()
+  })
 
-  test('shows same UI as expected', () => {
-    const { container } = render(<Chip text="Test Chip" />);
-    expect(container.firstChild).toMatchSnapshot();
-  });
+  test("shows same UI as expected", () => {
+    const { container } = render(<Chip text="Test Chip" />)
+    expect(container.firstChild).toMatchSnapshot()
+  })
 
-  test('changing props reflects on the UI', () => {
+  test("changing props reflects on the UI", () => {
     const { getByText, rerender } = render(
       <Chip text="Test Chip" variant="outlined" color="primary" disabled />
-    );
-    const chipText = getByText('Test Chip');
+    )
+    const chipText = getByText("Test Chip")
 
-    expect(chipText).toBeInTheDocument();
-    expect(chipText).not.toHaveClass('bg-primary-500');
-    expect(chipText).toHaveClass('border-primary-700');
-    expect(chipText).toHaveClass('text-primary-700');
-    expect(chipText).toBeDisabled();
+    expect(chipText).toBeInTheDocument()
+    expect(chipText).not.toHaveClass("bg-primary-500")
+    expect(chipText).toHaveClass("border-primary-700")
+    expect(chipText).toHaveClass("text-primary-700")
+    expect(chipText).toBeDisabled()
 
     // Re render with updated props
-    rerender(<Chip text="Updated Chip" variant="filled" color="primary" />);
+    rerender(<Chip text="Updated Chip" variant="filled" color="primary" />)
 
-    expect(chipText).toHaveTextContent('Updated Chip');
-    expect(chipText).toHaveClass('bg-primary-500');
-    expect(chipText).toHaveClass('text-white', 'bg-primary-500');
-    expect(chipText).not.toBeDisabled();
-  });
-});
+    expect(chipText).toHaveTextContent("Updated Chip")
+    expect(chipText).toHaveClass("bg-primary-500")
+    expect(chipText).toHaveClass("text-white", "bg-primary-500")
+    expect(chipText).not.toBeDisabled()
+  })
+})
---
+++ src/components/molecules/Breadcrumb/Group/BreadcrumbGroup.tsx
@@ -0,0 +1,71 @@
+import { IconChevronRight } from "@tabler/icons-react"
+import React, { FC, forwardRef, useState } from "react"
+import { text } from "../../../../theme/mapper"
+import Breadcrumb from "../Breadcrumb"
+import { getStyles } from "../utils/styles"
+import { BreadcrumbGroupProps } from "../utils/types"
+
+const BreadcrumbGroup: FC<BreadcrumbGroupProps> = forwardRef<
+  HTMLDivElement,
+  BreadcrumbGroupProps
+>(({ children, separator, color = "gray" }, ref) => {
+  const styles = getStyles({ color })
+  const numberOfItems = React.Children.count(children)
+
+  const [allVisible, setAllVisible] = useState(false)
+  separator = !separator ? (
+    <IconChevronRight className={`${text[color][300]}`} />
+  ) : (
+    separator
+  )
+  return (
+    <div ref={ref}>
+      <nav className={styles.groupNav}>
+        <ol className={styles.groupOrderList}>
+          {React.Children.map(children, (child, index) => {
+            const showSeparator = index + 1 < numberOfItems
+            const initialVisible = index <= 1 || index == numberOfItems - 1
+            const showDot = numberOfItems > 3 && index == 2
+
+            if (React.isValidElement<HTMLDivElement>(child)) {
+              const breadcrumbElement = React.cloneElement(child, {
+                ...child.props,
+                className: styles.groupItem,
+                key: index,
+                style: { ...child.props.style },
+              })
+
+              const expander = (
+                <li className={styles.groupList} key={"hello"}>
+                  <Breadcrumb
+                    className={styles.groupItem}
+                    onClick={() => setAllVisible(true)}
+                  >
+                    ...
+                  </Breadcrumb>
+                  {separator}
+                </li>
+              )
+
+              return (
+                <>
+                  <li
+                    className={`${styles.groupList} ${
+                      allVisible || initialVisible ? "" : "hidden"
+                    }`}
+                  >
+                    {breadcrumbElement}
+                    {showSeparator ? separator : null}
+                  </li>
+                  {showDot && !allVisible ? expander : null}
+                </>
+              )
+            }
+          })}
+        </ol>
+      </nav>
+    </div>
+  )
+})
+BreadcrumbGroup.displayName = "Breadcrumb.Group"
+export default BreadcrumbGroup
