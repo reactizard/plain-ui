9b22d9dbe1f61c1a6024b06e5bf8d593afa150af
LogicalAnt <shakil147258@gmail.com>
2023-08-29 17:26:38 +0600

    feat: :sparkles: add Toast component

---
+++ src/components/molecules/Toast/Toast.stories.tsx
@@ -0,0 +1,62 @@
+import { Meta, StoryObj } from "@storybook/react"
+import {
+  IconAlertCircleFilled,
+  IconAlertTriangleFilled,
+  IconDiscountCheckFilled,
+} from "@tabler/icons-react"
+import React from "react"
+import { text } from "../../../theme/mapper"
+import Link from "../../atoms/Link"
+import Toast from "./Toast"
+
+const icons = {
+  none: "",
+  AlertTriangle: <IconAlertTriangleFilled className={text["danger"][500]} />,
+  Alert: <IconAlertCircleFilled className={text["warning"][500]} />,
+  DiscountCheck: <IconDiscountCheckFilled className={text["success"][500]} />,
+}
+
+const meta: Meta<typeof Toast> = {
+  title: "molecules/Toast",
+  component: Toast,
+  tags: ["autodocs"],
+}
+
+type Story = StoryObj<typeof Toast>
+
+export const Primary: Story = {
+  args: {
+    color: "success",
+    variant: "filled",
+    title: "Toast",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
+    closable: true,
+    autoClose: true,
+    sideIcon: <IconDiscountCheckFilled className={text["success"][500]} />,
+    footer: <Link href="http://www.google.com" text="Search" target="_blank" />,
+    onClose: (e) => console.log("The Toast has been closed", e.type),
+  },
+  argTypes: {
+    autoClose: { description: "After 10 second" },
+    onClose: { control: false },
+    footer: { type: "string" || "function" },
+    sideIcon: {
+      options: Object.keys(icons),
+      mapping: icons,
+      control: {
+        type: "select",
+      },
+    },
+  },
+}
+export const minimal: Story = {
+  args: {
+    variant: "filled",
+    title: "Lorem ipsum dolor sit amet",
+    color: "success",
+    closable: true,
+    autoClose: false,
+  },
+}
+
+export default meta
