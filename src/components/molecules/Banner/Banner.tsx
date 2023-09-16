0cc0613e222e63a1de34e21bd384ebe2049d18d9
LogicalAnt <shakil147258@gmail.com>
2023-09-16 11:06:12 +0600

    refactor: :recycle: memoize getStyle function, remove dismissable node if not present

--- src/components/molecules/Banner/Banner.tsx
+++ src/components/molecules/Banner/Banner.tsx
@@ -1,18 +1,26 @@
-import React, { FC, forwardRef } from "react"
+import React, { FC, forwardRef, useMemo } from "react"
 import { Void } from "../../../icons"
 import { getStyles } from "./utils/styles"
 import { BannerProps } from "./utils/types"
+
+const DefaultIcon = <Void />
+
 export const Banner: FC<BannerProps> = forwardRef<HTMLDivElement, BannerProps>(
-  function Banner({ color, variant, dismissable, icon, subtitle, title }, ref) {
-    const styles = getStyles({ color, variant })
+  function Banner(
+    { color, dismissable, icon = DefaultIcon, subtitle, title },
+    ref
+  ) {
+    const styles = useMemo(() => getStyles({ color }), [color])
     return (
       <div ref={ref} className={styles.container}>
-        <div className={styles.icon}>{icon ? icon : <Void />}</div>
+        <div className={styles.icon}>{icon}</div>
         <div className={styles.body}>
           <p className={styles.title}>{title}</p>
           <p className={styles.subtitle}>{subtitle}</p>
         </div>
-        <div className={styles.dismissable}>{dismissable}</div>
+        {dismissable ? (
+          <div className={styles.dismissable}>{dismissable}</div>
+        ) : null}
       </div>
     )
   }
