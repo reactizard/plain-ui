01d88fac51e68989bcc926fa5300bbb8e926e83d
LogicalAnt <shakil147258@gmail.com>
2023-09-27 20:44:43 +0600

    docs: :memo: show code for group avatar implementation

--- src/components/atoms/Avatar/Avatar.stories.tsx
+++ src/components/atoms/Avatar/Avatar.stories.tsx
@@ -1,6 +1,8 @@
 import { Meta, StoryObj } from "@storybook/react"
 import React from "react"
 import Avatar from "./Avatar"
+import AvatarGroup from "./Group/AvatarGroup"
+import { AvatarProps } from "./utils/types"
 const meta: Meta<typeof Avatar> = {
   title: "Atoms/Avatar",
   component: Avatar,
@@ -8,34 +10,43 @@ const meta: Meta<typeof Avatar> = {
 }
 
 type Story = StoryObj<typeof Avatar>
+type AvatarGroupStory = StoryObj<typeof AvatarGroup>
 
 export const primary: Story = {
   args: {
     size: "md",
     children: "Adam smith",
+    onlineStatus: "online",
     imgURL: "https://avatars.githubusercontent.com/u/16508504?v=4",
   },
 }
 
-const GroupTemplate: Story = {
-  argTypes: {},
-  render: () => {
+const GroupTemplate: AvatarGroupStory = {
+  argTypes: { more: Number },
+  args: { more: 9 },
+  render: ({ items, ...args }: any) => {
     return (
-      <Avatar.Group more="10">
-        <Avatar>Hello</Avatar>
-        <Avatar
-          onlineStatus="online"
-          imgURL="https://avatars.githubusercontent.com/u/16508504?v=4"
-        />
-        <Avatar>Adam Smith</Avatar>
-        <Avatar>Hello</Avatar>
+      <Avatar.Group {...args}>
+        {items.map((item: object, index: number) => {
+          return <Avatar {...item} key={index} />
+        })}
       </Avatar.Group>
     )
   },
 }
 
+const AvatarItemProps: AvatarProps[] = [
+  { children: "Hello" },
+  {
+    onlineStatus: "online",
+    imgURL: "https://avatars.githubusercontent.com/u/16508504?v=4",
+  },
+  { children: "Adam Smith" },
+  { children: "Robert Bruce" },
+]
 export const GroupAvatar = {
   ...GroupTemplate,
+  args: { items: AvatarItemProps },
 }
 
 export default meta
