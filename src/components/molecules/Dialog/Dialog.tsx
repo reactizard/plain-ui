097c9b6464fd77d7b9c1d20839a8e49bd298615c
LogicalAnt <shakil147258@gmail.com>
2023-11-14 02:28:20 +0600

    feat: :sparkles: add Dialog component

--- src/components/molecules/Dialog/Dialog.tsx
+++ src/components/molecules/Dialog/Dialog.tsx
@@ -1,7 +1,54 @@
-import React from "react"
+import { IconX } from "@tabler/icons-react"
+import React, { FC, forwardRef, useEffect } from "react"
+import FadeMotion from "../hoc/motion/FadeMotion"
+import { getStyles } from "./utils/styles"
+import { DialogProps } from "./utils/types"
 
-const Dialog = () => {
-  return <div>Dialog</div>
-}
+export const Dialog: FC<DialogProps> = forwardRef<HTMLDivElement, DialogProps>(
+  function Dialog(
+    {
+      onClose,
+      open = false,
+      actions,
+      content,
+      title,
+      positionX,
+      positionY,
+      icon,
+      size = "md",
+    },
+    ref
+  ) {
+    const styles = getStyles({ positionX, positionY, size })
+    useEffect(() => {
+      const handleEsc = (event: any) => {
+        if (event.key === "Escape" && onClose) {
+          onClose()
+        }
+      }
+
+      window.addEventListener("keydown", handleEsc)
+      return () => {
+        window.removeEventListener("keydown", handleEsc)
+      }
+    }, [onClose])
+
+    const actionBtns = <div className={styles.actionContainer}>{actions}</div>
+    return (
+      <FadeMotion shouldFade={open} classes={styles.motionContainer}>
+        <div ref={ref} className={styles.container}>
+          <div className={styles.iconContainer}>
+            <div className="icon">{icon}</div>
+            <IconX className={styles.closeIcon} onClick={onClose} />
+          </div>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.content}>{content}</div>
+
+          {actionBtns}
+        </div>
+      </FadeMotion>
+    )
+  }
+)
 
 export default Dialog
---
+++ src/components/molecules/Dialog/utils/styles.ts
@@ -0,0 +1,84 @@
+import classNames from "classnames"
+import { Position } from "../../../../theme/constants"
+import { StyleProps } from "./types"
+
+export const getStyles = ({ size, positionX, positionY }: StyleProps) => {
+  const sizeMapper: Record<NonNullable<StyleProps["size"]>, string> = {
+    sm: "w-[300px]",
+    md: "w-[400px]",
+    lg: "w-[600px]",
+  }
+  const positionMapper: Record<Position, string> = {
+    bottom: "bottom-5",
+    top: "top-5",
+    left: "left-5",
+    right: "right-5",
+    center: "center-5",
+  }
+  const isOppositePositions =
+    (positionX == "top" && positionY == "bottom") ||
+    (positionX == "bottom" && positionY == "top")
+
+  const motionContainer = classNames([
+    "flex items-center justify-center fixed inset-0 z-[9999] bg-black bg-opacity-20",
+  ])
+  const container = classNames([
+    size ? sizeMapper[size] : "",
+    "absolute",
+    !isOppositePositions ? positionMapper[positionX ?? "center"] : "",
+    positionMapper[positionY ?? "center"],
+    "p-5",
+    "bg-white",
+    "rounded-lg",
+    "shadow border",
+    "border-gray-200",
+    "flex-col",
+    "justify-start",
+    "items-start",
+    "gap-4",
+    "inline-flex",
+  ])
+  const actionContainer = classNames([
+    //"w-[352px]",
+    //"h-10",
+    "w-full",
+    "justify-start",
+    "items-center",
+    "gap-4",
+    "inline-flex",
+  ])
+  const iconContainer = classNames([
+    "icons",
+    "flex",
+    "flex-row",
+    "justify-between",
+    "w-full",
+  ])
+  const closeIcon = classNames([
+    "cursor-pointer",
+    "stroke-slate-600",
+    "w-5",
+    "h-5",
+  ])
+  const title = classNames([
+    "text-gray-900",
+    "text-sm",
+    "font-semibold",
+    "font-['Inter']",
+    "leading-snug",
+  ])
+  const content = classNames([
+    "text-gray-500",
+    "text-smRegular",
+    "leading-snug",
+  ])
+  return {
+    container,
+    motionContainer,
+    actionContainer,
+    iconContainer,
+    closeIcon,
+    title,
+    content,
+  }
+}
