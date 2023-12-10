e894c6e74ed71e83a44a404b025e877caf49fb98
LogicalAnt <shakil147258@gmail.com>
2023-12-11 03:00:57 +0600

    feat: :zap: add collapsible icon  and  data to Accordion props

--- src/components/molecules/Accordion/utils/types.ts
+++ src/components/molecules/Accordion/utils/types.ts
@@ -1,8 +1,8 @@
-
-
 import { Colors } from "../../../../theme/constants"
 
 export interface AccordionProps {
+  data: any
   color?: Colors
+  collapseIcon?: JSX.Element
 }
 export type StyleProps = Pick<AccordionProps, "color">
