0ddcdf91c4320ad04cb8aa3adcd1a7d34e25c54c
LogicalAnt <shakil147258@gmail.com>
2023-12-15 02:11:50 +0600

    feat: :sparkles: add size prop to accordion component

--- src/components/molecules/Accordion/utils/types.ts
+++ src/components/molecules/Accordion/utils/types.ts
@@ -1,8 +1,9 @@
-import { Colors } from "../../../../theme/constants"
+import { Colors, Size } from "../../../../theme/constants"
 
 export interface AccordionProps {
   data: any
   color?: Colors
   collapseIcon?: JSX.Element
+  size?: Size
 }
-export type StyleProps = Pick<AccordionProps, "color">
+export type StyleProps = Pick<AccordionProps, "color" | "size">
