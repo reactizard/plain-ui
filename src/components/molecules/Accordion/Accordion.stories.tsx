b376ba4bcd37a9f3ac0ce2c63183e22c27bbb161
LogicalAnt <shakil147258@gmail.com>
2023-12-25 18:24:42 +0600

    feat: :sparkles: add control to toggle multiple accordion item

--- src/components/molecules/Accordion/Accordion.stories.tsx
+++ src/components/molecules/Accordion/Accordion.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react"
 import {
   IconArrowDown,
+  IconArrowDownLeft,
   IconCaretDownFilled,
   IconPlus,
 } from "@tabler/icons-react"
@@ -8,25 +9,33 @@ import React from "react"
 import { text } from "../../../theme/mapper"
 import { Rating } from "../../atoms/Rating"
 import Accordion from "./Accordion"
+import { AccordionDataShape } from "./utils/types"
 
 const icons = {
   none: "",
   Plus: <IconPlus className={text["danger"][500]} />,
   Down: <IconArrowDown className={text["warning"][500]} />,
   CaretDown: <IconCaretDownFilled className="h-5 text-success-500" />,
+  ArcheryArrow: <IconArrowDownLeft />,
 }
 
 const meta: Meta<typeof Accordion> = {
   title: "molecules/Accordion",
   component: Accordion,
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div className="flex items-center justify-center h-[400px]">
+        <Story />
+      </div>
+    ),
+  ],
 }
 
 type Story = StoryObj<typeof Accordion>
 
-const data = [
+const data: AccordionDataShape[] = [
   {
-    key: "1",
     label: "This is panel header 1",
     children: (
       <p>There are many variations of passages of Lorem Ipsum available,</p>
@@ -34,7 +43,6 @@ const data = [
     disabled: true,
   },
   {
-    key: "2",
     label: "This is panel header 2",
     children: (
       <div>
@@ -48,27 +56,16 @@ const data = [
     ),
   },
   {
-    key: "3",
     label: "This is panel header 3",
-    children: (
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-    ),
+    children:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   },
 ]
 
 export const primary: Story = {
   args: {
     data: data,
+    multiActive: false,
   },
   argTypes: {
     collapseIcon: {
--- src/components/molecules/Accordion/utils/types.ts
+++ src/components/molecules/Accordion/utils/types.ts
@@ -1,9 +1,16 @@
+import { ReactNode } from "react"
 import { Colors, Size } from "../../../../theme/constants"
 
+export interface AccordionDataShape {
+  label?: string
+  children?: ReactNode
+  disabled?: boolean
+}
 export interface AccordionProps {
   data: any
   color?: Colors
   collapseIcon?: JSX.Element
   size?: Size
+  multiActive?: boolean
 }
 export type StyleProps = Pick<AccordionProps, "color" | "size">
