8a944a9dd10f5fe7fbe582e8376264003d817d38
LogicalAnt <shakil147258@gmail.com>
2023-12-25 18:25:17 +0600

    refactor: :recycle: format and organize styles

--- src/components/molecules/Accordion/utils/styles.ts
+++ src/components/molecules/Accordion/utils/styles.ts
@@ -3,43 +3,50 @@ import { bg } from "../../../../theme/mapper"
 import { StyleProps } from "./types"
 
 export const getStyles = ({ color, size }: StyleProps) => {
-  const sizeMapper: Record<NonNullable<StyleProps["size"]>, string> = {
-    lg: "w-[800px]",
-    md: "w-[600px]",
-    sm: "w-[400px]",
+  const sizeMapper: Record<
+    NonNullable<StyleProps["size"]>,
+    { head: string; body: string }
+  > = {
+    lg: { body: "w-[700px]", head: "p-4" },
+    md: { body: "w-[600px]", head: "p-3" },
+    sm: { body: "w-[400px]", head: "p-1" },
   }
-  const container = classNames([size ? sizeMapper[size] : "", "mx-auto"])
+  const container = classNames([
+    size ? sizeMapper[size].body : "",
+    "mx-auto",
+    "flex",
+    "flex-col",
+    "gap-2",
+  ])
   const itemContainer = classNames(["mb-0", "group"])
   const itemHeader = classNames([
-    "bg-gray-200",
-    "p-3",
+    size ? sizeMapper[size].head : "",
     "flex",
     "items-center",
     "justify-between",
     color ? bg[color][200] : "",
   ])
 
-  const haderLabel = classNames(["font-bold"])
+  const haderLabel = classNames(["text-lgSemiBold"])
   const toggleIcon = classNames([
     "transition-transform",
     "ease-in-out",
-    "duration-300 transform",
+    "duration-500",
+    "transform",
     "group-hover:translate-y-1",
   ])
 
   const contentContainer = classNames([
-    "p-[5px]",
     "rounded-b",
     "overflow-hidden",
     "transition-max-height",
     "ease-in-out",
     "duration-500",
-    ,
   ])
   const activeClasses = {
     itemHeader: classNames(["rounded-t"]),
     toggleIcon: classNames(["rotate-180"]),
-    contentContainer: classNames(["shadow-lg max-h-screen"]),
+    contentContainer: classNames(["shadow-lg", "max-h-screen", "p-3"]),
   }
   const inactiveClasses = {
     itemHeader: classNames(["rounded"]),
