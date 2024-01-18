a9ec0fcddaed7deb0ba24073d54fbf0ba5410c50
LogicalAnt <shakil147258@gmail.com>
2024-01-18 21:38:02 +0600

    test: :test_tube: add initial test for dialog component

--- src/components/molecules/Dialog/Dialog.test.tsx
+++ src/components/molecules/Dialog/Dialog.test.tsx
@@ -1,10 +1,26 @@
-
 import "@testing-library/jest-dom/extend-expect"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import React from "react"
 
 import { Dialog } from "./Dialog"
 
 describe("Dialog component", () => {
-  test("renders properly", () => {})
+  it("should render a dialog with custom props when passed valid props", () => {
+    const customTitle = "Custom Title"
+    const customContent = "Custom Content"
+    const customAction = "Custom Action"
+
+    render(
+      <Dialog
+        open={true}
+        title={customTitle}
+        content={customContent}
+        actions={[customAction]}
+      />
+    )
+
+    expect(screen.getByText(customTitle)).toBeInTheDocument()
+    expect(screen.getByText(customContent)).toBeInTheDocument()
+    expect(screen.getByText(customAction)).toBeInTheDocument()
+  })
 })
