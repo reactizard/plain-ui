d899389109fa34389fc59d135ec6be1c3e4960a0
LogicalAnt <shakil147258@gmail.com>
2024-07-06 02:47:21 +0600

    feat: :sparkles: add initial dropdown component

--- src/components/molecules/Dropdown/Dropdown.tsx
+++ src/components/molecules/Dropdown/Dropdown.tsx
@@ -1,11 +1,70 @@
-
-import React, { FC, forwardRef, useEffect, useRef } from "react"
+import React, { FC, forwardRef, useRef, useState } from "react"
 import { DropdownProps } from "./utils/types"
 import { getStyles } from "./utils/styles"
 
-export const Dropdown: FC<DropdownProps> = forwardRef<HTMLDivElement, DropdownProps>(
-  function Dropdown({ color = "primary" },ref) {
-    const styles = getStyles({ color })
-    return (<div ref={ref}>Dropdown</div>)
+import FadeMotion from "../hoc/motion/FadeMotion"
+import { useAccessibility } from "./utils/useAccessibility"
+import { twMerge } from "tailwind-merge"
+import useSafePosition from "./utils/useSafePosition"
+
+export const Dropdown: FC<DropdownProps> = forwardRef<
+  HTMLDivElement,
+  DropdownProps
+>(function Dropdown(
+  { width = 250, items = [], opener, onItemClick = null },
+  ref
+) {
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedItem, setSelectedItem] = useState<number>(-1)
+  const styles = getStyles()
+  const newRef = useRef(null)
+  const containerRef = ref ? ref : newRef
+  const openerRef = useRef(null)
+
+  useAccessibility({
+    isOpen,
+    setIsOpen,
+    ref: containerRef as React.MutableRefObject<null>,
+    openerRef,
+  })
+
+  return (
+    <div className={styles.container} ref={containerRef}>
+      {opener && opener({ isOpen, setIsOpen, ref: openerRef })}
+
+      <FadeMotion shouldFade={isOpen}>
+        <ul
+          className={styles.itemsContainer}
+          style={{ width: width }}
+          role="menu"
+          aria-haspopup="true"
+          tabIndex={-1}
+        >
+          {items.map((item, index) => (
+            <li
+              key={item.key}
+              className={twMerge(
+                styles.item,
+                index == selectedItem &&
+                  "focus:outline-none  focus:bg-gray-200",
+                item.label && "hover:bg-gray-200"
+              )}
+              onClick={(e) => {
+                if (item.label) {
+                  setSelectedItem(index)
+                  if (onItemClick) onItemClick(e)
+                }
+              }}
+              role="menuitem"
+              tabIndex={0}
+            >
+              {item.extraElement}
+              {item.label}
+            </li>
+          ))}
+        </ul>
+      </FadeMotion>
+    </div>
+  )
 })
 export default Dropdown
