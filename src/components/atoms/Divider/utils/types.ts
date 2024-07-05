0feb741a13f74c67e99f570795c76aa498d61c7f
LogicalAnt <shakil147258@gmail.com>
2024-07-06 02:39:22 +0600

    feat: :sparkles: divider can be horizontal line now

--- src/components/atoms/Divider/utils/types.ts
+++ src/components/atoms/Divider/utils/types.ts
@@ -2,8 +2,8 @@ import { ReactNode } from "react"
 import { Colors, Position, Size } from "../../../../theme/constants"
 
 export interface DividerProps {
-  label: string | ReactNode
-  labelPosition: Exclude<Position, "top" | "bottom">
+  label?: string | ReactNode
+  labelPosition?: Exclude<Position, "top" | "bottom">
   color?: Colors
   weight?: Size
 }
