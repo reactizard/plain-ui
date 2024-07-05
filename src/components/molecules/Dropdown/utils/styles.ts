0161ebf7163716e0333ad92f8854c911f389418f
LogicalAnt <shakil147258@gmail.com>
2024-07-06 02:47:21 +0600

    feat: :sparkles: add initial dropdown component

--- src/components/molecules/Dropdown/utils/styles.ts
+++ src/components/molecules/Dropdown/utils/styles.ts
@@ -1,9 +1,12 @@
-
-
 import classNames from "classnames"
-import { StyleProps } from "./types"
 
-export const getStyles = ({ color }: StyleProps) => {
-  const container = classNames([""])
-  return { container }
+export const getStyles = () => {
+  const container = classNames(["relative inline-block"])
+  const itemsContainer = classNames([
+    "absolute rounded-md overflow-scroll p-1 z-10 shadow-md bg-white mt-1",
+  ])
+  const item = classNames([
+    "cursor-pointer items-center rounded-md flex gap-1 p-1 font-inter text-smRegular h-auto",
+  ])
+  return { container, itemsContainer, item }
 }
