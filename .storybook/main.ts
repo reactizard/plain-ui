df88d47b08858f6de48dc5344a3d449eba25c4bb
LogicalAnt <shakil147258@gmail.com>
2024-07-06 02:32:50 +0600

    chore: :package: upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,31 @@
 import type { StorybookConfig } from "@storybook/react-webpack5"
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
     {
       name: "@storybook/addon-styling",
       options: {
         postCss: true,
       },
     },
+    "@storybook/addon-webpack5-compiler-babel",
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 }
 export default config
