ff8ce2b515d4073017f30c1e4547101aefc8e6b2
LogicalAnt <shakil147258@gmail.com>
2024-07-06 02:34:37 +0600

    feat: :sparkles: add ring tailwind class mapper

---
+++ src/theme/mapper/ring.ts
@@ -0,0 +1,45 @@
+const ring = {
+  primary: {
+    [300]: "ring-primary-300",
+  },
+  gray: {
+    [300]: "ring-gray-300",
+  },
+  success: {
+    [300]: "ring-success-300",
+  },
+  warning: {
+    [300]: "ring-warning-300",
+  },
+  danger: {
+    [300]: "ring-danger-300",
+  },
+  yellow: {
+    [300]: "ring-yellow-300",
+  },
+  orange: {
+    [300]: "ring-orange-300",
+  },
+  melon: {
+    [300]: "ring-melon-300",
+  },
+  pink: {
+    [300]: "ring-pink-300",
+  },
+  purple: {
+    [300]: "ring-purple-300",
+  },
+  azure: {
+    [300]: "ring-azure-300",
+  },
+  aloha: {
+    [300]: "ring-aloha-300",
+  },
+  lime: {
+    [300]: "ring-lime-300",
+  },
+  warm: {
+    [300]: "ring-warm-300",
+  },
+}
+export default ring
