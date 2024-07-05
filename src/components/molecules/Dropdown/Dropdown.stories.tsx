f11333f14a67262d1ead3fb0c037174dea806c68
LogicalAnt <shakil147258@gmail.com>
2024-07-06 02:47:21 +0600

    feat: :sparkles: add initial dropdown component

--- src/components/molecules/Dropdown/Dropdown.stories.tsx
+++ src/components/molecules/Dropdown/Dropdown.stories.tsx
@@ -1,19 +1,153 @@
-
 import { Meta, StoryObj } from "@storybook/react"
-import React from "react"
+import React, { SyntheticEvent } from "react"
 import Dropdown from "./Dropdown"
+import {
+  IconUser,
+  IconBuilding,
+  IconNotification,
+  IconCreditCard,
+  IconLogout,
+  IconLanguage,
+  IconChevronUp,
+  IconChevronDown,
+} from "@tabler/icons-react"
+import { DropdownProps, MenuItemShape } from "./utils/types"
+import Button from "../../atoms/Button"
+import Avatar from "../../atoms/Avatar"
+import Divider from "../../atoms/Divider"
+
+const items: MenuItemShape[] = [
+  {
+    key: "i1",
+    label: "Your Profile",
+    extraElement: <IconUser size={15} />,
+  },
+  {
+    key: "i2",
+    label: "Your Organization",
+    extraElement: <IconBuilding size={15} />,
+  },
+  {
+    key: "i3",
+    extraElement: <Divider label="Settings" />,
+  },
+  {
+    key: "i4",
+    label: "Notifications",
+    extraElement: <IconNotification size={15} />,
+  },
+  {
+    key: "i5",
+    label: "Payment",
+    extraElement: <IconCreditCard size={15} />,
+  },
+  {
+    key: "i6",
+    label: "Language",
+    extraElement: <IconLanguage size={15} />,
+  },
+  {
+    key: "i7",
+    extraElement: <Divider label="" />,
+  },
+  {
+    key: "i8",
+    label: "Log Out",
+    extraElement: <IconLogout size={15} />,
+  },
+]
 const meta: Meta<typeof Dropdown> = {
   title: "molecules/Dropdown",
   component: Dropdown,
   tags: ["autodocs"],
+  argTypes: {
+    onItemClick: { type: "function" },
+    width: { description: "number of pixel" },
+  },
+  decorators: [
+    (Story) => (
+      <div className="flex items-center justify-center h-[300px] flex-col">
+        <Story />
+      </div>
+    ),
+  ],
 }
 
 type Story = StoryObj<typeof Dropdown>
 
-export const primary: Story = {
+const opnr: DropdownProps["opener"] = function opener({ isOpen, setIsOpen }) {
+  return (
+    <Avatar
+      imgURL=""
+      onlineStatus="online"
+      size="md"
+      onClick={() => setIsOpen(!isOpen)}
+      style={{ cursor: "pointer" }}
+    >
+      Adam Smith
+    </Avatar>
+  )
+}
+
+export const avatarOpener: Story = {
+  name: "Avatar Opener",
   args: {
-    color:"primary"
+    items: items,
+    opener: opnr,
   },
+  decorators: [
+    (Story) => (
+      <div className="flex items-center justify-center h-[300px] flex-col">
+        <Story />
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+          ever since the 1500s, when an unknown printer took a galley of type
+          and scrambled it to make a type specimen book.
+        </p>
+      </div>
+    ),
+  ],
+}
+
+export const ButtonOpener = () => {
+  const openerElement: DropdownProps["opener"] = ({
+    isOpen,
+    setIsOpen,
+    ref,
+  }) => (
+    <Button
+      onClick={() => setIsOpen(!isOpen)}
+      rightIcon={isOpen ? <IconChevronUp /> : <IconChevronDown />}
+      ref={ref}
+    >
+      Click
+    </Button>
+  )
+
+  const onClick = (e: SyntheticEvent) => {
+    console.log((e.target as HTMLLIElement).innerText)
+  }
+
+  return <Dropdown items={items} opener={openerElement} onItemClick={onClick} />
+}
+
+export const HoverOpener = () => {
+  const openerElement: DropdownProps["opener"] = ({
+    isOpen,
+    setIsOpen,
+    ref,
+  }) => (
+    <Button
+      onMouseOver={() => setIsOpen(true)}
+      rightIcon={isOpen ? <IconChevronUp /> : <IconChevronDown />}
+      ref={ref}
+    >
+      Hover Over
+    </Button>
+  )
+
+  return <Dropdown items={items} opener={openerElement} />
 }
 
 export default meta
--- src/components/molecules/Dropdown/utils/types.ts
+++ src/components/molecules/Dropdown/utils/types.ts
@@ -1,8 +1,19 @@
+import { RefObject } from "react"
 
-
-import { Colors } from "../../../../theme/constants"
-
-export interface DropdownProps {
-  color?: Colors
+export interface MenuItemShape {
+  key: string
+  label?: string
+  extraElement?: React.ReactElement
 }
-export type StyleProps = Pick<DropdownProps, "color">
+interface OpenerProps {
+  isOpen: boolean
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+  ref: RefObject<any>
+}
+export interface DropdownProps {
+  width?: number | string
+  items?: MenuItemShape[]
+  opener?: ({ isOpen, setIsOpen, ref }: OpenerProps) => React.ReactNode
+  onItemClick?: (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => void
+}
+export type StyleProps = Pick<DropdownProps, "items">
