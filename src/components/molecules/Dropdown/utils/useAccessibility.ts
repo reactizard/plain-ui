2f8264edeab21fd8d9e32505585f95634224a10e
LogicalAnt <shakil147258@gmail.com>
2024-07-06 02:47:54 +0600

    feat: :sparkles: add initial accessibility feature for dropdown

---
+++ src/components/molecules/Dropdown/utils/useAccessibility.ts
@@ -0,0 +1,45 @@
+import { RefObject, useEffect } from "react"
+
+interface DropDownAccessibilityProps {
+  isOpen: boolean
+  setIsOpen: (open: boolean) => void
+  ref?: RefObject<any>
+  openerRef?: RefObject<HTMLElement>
+}
+export const useAccessibility = ({
+  isOpen,
+  setIsOpen,
+  ref,
+  openerRef,
+}: DropDownAccessibilityProps) => {
+  const handleKeydown = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case "Escape":
+      case "Tab":
+        event.preventDefault()
+        setIsOpen(false)
+        if (openerRef && openerRef.current) openerRef.current.focus()
+        break
+
+      default:
+        break
+    }
+  }
+
+  const handleClickOutside = (event: Event) => {
+    if (ref && ref.current && !ref.current.contains(event.target)) {
+      setIsOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("keydown", handleKeydown)
+      document.addEventListener("mousedown", handleClickOutside)
+      return () => {
+        document.removeEventListener("keydown", handleKeydown)
+        document.removeEventListener("mousedown", handleClickOutside)
+      }
+    }
+  }, [isOpen])
+}
