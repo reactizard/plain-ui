c651b440d54a6afa3d2b425f168db30885e74c82
LogicalAnt <shakil147258@gmail.com>
2024-08-01 04:51:12 +0600

    feat: :sparkles: use icon props value

--- src/components/atoms/Input/Input.stories.tsx
+++ src/components/atoms/Input/Input.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react"
 import React from "react"
 import Input from "./Input"
+import { IconAlertCircle, IconMessage } from "@tabler/icons-react"
 const meta: Meta<typeof Input> = {
   title: "atoms/Input",
   component: Input,
@@ -13,6 +14,8 @@ export const primary: Story = {
   args: {
     color: "primary",
     placeholder: "Email Email Email Email Email Email",
+    leftIcon: <IconMessage />,
+    rightIcon: <IconAlertCircle />,
   },
 }
 
