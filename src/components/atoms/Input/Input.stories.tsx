cda2c66329d0bcac0bc0099c6ea6529a905d2ed8
LogicalAnt <shakil147258@gmail.com>
2024-08-11 13:08:21 +0600

    refactor: :recycle: change password story icon

--- src/components/atoms/Input/Input.stories.tsx
+++ src/components/atoms/Input/Input.stories.tsx
@@ -1,7 +1,12 @@
 import { Meta, StoryObj } from "@storybook/react"
 import React from "react"
 import Input from "./Input"
-import { IconAlertCircle, IconMail, IconMessage } from "@tabler/icons-react"
+import {
+  IconAlertCircle,
+  IconLock,
+  IconMail,
+  IconMessage,
+} from "@tabler/icons-react"
 import { text } from "../../../theme/mapper"
 const meta: Meta<typeof Input> = {
   title: "atoms/Input",
@@ -14,9 +19,10 @@ type Story = StoryObj<typeof Input>
 export const primary: Story = {
   args: {
     color: "primary",
-    placeholder: "Email Email Email Email Email Email",
-    leftIcon: <IconMessage size={18} color="gray" />,
-    rightIcon: <IconAlertCircle size={18} color="gray" />,
+    placeholder: "Email",
+    leftIcon: <IconMessage size={18} />,
+    rightIcon: <IconAlertCircle size={18} />,
+    label: "Enter your email address",
   },
 }
 
@@ -24,6 +30,7 @@ export const password: Story = {
   args: {
     type: "password",
     placeholder: "Password",
+    leftIcon: <IconLock size={18} />,
   },
 }
 
