2420b56fe8083727ea1dc1271b73427514570bef
LogicalAnt <shakil147258@gmail.com>
2024-08-19 23:13:06 +0600

    feat: :sparkles: label can take different types of weights

--- src/components/atoms/Label/Label.stories.tsx
+++ src/components/atoms/Label/Label.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react"
 import Label from "./Label"
+import React from "react"
 
 const meta: Meta<typeof Label> = {
   title: "Atoms/Label",
@@ -14,5 +15,26 @@ export const Primary: Story = {
   args: {
     color: undefined,
     text: "Hello There",
+  },
+}
+
+export const Labels: Story = {
+  render: () => {
+    return (
+      <div className="flex flex-col">
+        <Label text="xsRegular" weight="xs" />
+        <Label text="smRegular" weight="sm" />
+        <Label text="mdRegular" weight="md" />
+        <Label text="lgRegular" weight="lg" />
+        <Label text="xsSemiBold" weight="xs-med" />
+        <Label text="smSemiBold" weight="sm-med" />
+        <Label text="mdSemiBold" weight="md-med" />
+        <Label text="lgSemiBold" weight="lg-med" />
+        <Label text="h4Bold" weight="h4" />
+        <Label text="h3Bold" weight="h3" />
+        <Label text="h2Bold" weight="h2" />
+        <Label text="h1Bold" weight="h1" />
+      </div>
+    )
   },
 }
--- src/components/atoms/Label/utils/types.ts
+++ src/components/atoms/Label/utils/types.ts
@@ -1,8 +1,13 @@
+import { HTMLProps } from "react"
 import { Colors, LabelVariant } from "../../../../theme/constants"
+import { FontWeights } from "../../../../theme/constants/font"
 
-export interface LabelProps {
+export interface LabelProps extends HTMLProps<HTMLLabelElement> {
   color?: Colors | undefined
   text: string
   variant?: LabelVariant
   labelFor?: string
+  weight?: FontWeights
 }
+
+export type StyleProps = Pick<LabelProps, "color" | "variant" | "weight">
