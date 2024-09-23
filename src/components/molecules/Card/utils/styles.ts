59dc14f2c621a45067b075f547faaa86ef2a1ab8
LogicalAnt <shakil147258@gmail.com>
2024-09-24 00:31:47 +0600

    style: :lipstick: add styling for media type card component

--- src/components/molecules/Card/utils/styles.ts
+++ src/components/molecules/Card/utils/styles.ts
@@ -1,5 +1,7 @@
 interface StyleProps {
   align?: "left" | "center" | "right"
+  media?: boolean
+  itemTypes?: ("media" | "text")[]
 }
 
 const alignMapper = {
@@ -7,7 +9,10 @@ const alignMapper = {
   center: "items-center justify-center",
   right: "items-end justify-end",
 }
-export const getStyles = ({ align }: StyleProps) => {
+export const getStyles = ({ align, media, itemTypes }: StyleProps) => {
+  const topItemIsMedia = itemTypes?.[0] === "media"
+  const bottomItemIsMedia = itemTypes?.[itemTypes.length - 1] === "media"
+
   const groupContainer =
     "flex flex-row w-fit shadow-md hover:shadow-lg border rounded-lg"
   const groupDivider = "w-10 h-auto my-5"
@@ -17,11 +22,17 @@ export const getStyles = ({ align }: StyleProps) => {
     middle: "rounded-none",
   }
 
-  const contentContainer = ["flex", align && alignMapper[align]]
+  const contentContainer = [
+    "flex",
+    media ? "px-0" : "px-4",
+    align && alignMapper[align],
+  ]
 
   const cardContainer = [
-    "flex flex-col gap-4  box-content  px-4 py-10 overflow-scroll",
+    "flex flex-col gap-4  box-content mx-2 py-10 overflow-scroll",
     align && alignMapper[align],
+    topItemIsMedia ? "pt-0" : "pt-10",
+    bottomItemIsMedia ? "pb-0" : "pb-10",
   ]
 
   const cardContainerNotGrouped = [
