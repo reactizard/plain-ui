422895816258573547559d373e96fbbf7b4a51ff
LogicalAnt <shakil147258@gmail.com>
2024-09-24 00:31:47 +0600

    style: :lipstick: add styling for media type card component

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -5,12 +5,21 @@ import { getStyles } from "./utils/styles"
 import { Props } from "./utils/types"
 
 export const Card = forwardRef(function Card(
-  { children, width = 0, height = 0, align = "left" }: Props,
+  { children, width = 0, height = 0, align = "left", ...props }: Props,
   ref: Ref<HTMLDivElement>
 ) {
   const cardContext = React.useContext(CardContext)
   const { group } = cardContext
-  const styles = getStyles({ align })
+
+  const childrenArray = React.Children.toArray(children)
+  const itemTypes = childrenArray.map((child) => {
+    if (React.isValidElement(child)) {
+      return child.props.media ? "media" : "text"
+    }
+    return "text"
+  })
+
+  const styles = getStyles({ align, itemTypes })
 
   const groupFirstItem = group?.current ?? null
   const groupLastItem = (group && group?.total - 1) ?? null
@@ -28,7 +37,8 @@ export const Card = forwardRef(function Card(
         className={twMerge(
           groupStyle,
           !groupStyle ? styles.cardContainerNotGrouped : "",
-          styles.cardContainer
+          styles.cardContainer,
+          props.className
         )}
         style={{
           width: width || "40vw",
